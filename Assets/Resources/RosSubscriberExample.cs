d6f2e7c2259e396459900ddcb67b2d8ae031f38c
Phil Schneider <Phil.Schneider@fh-zwickau.de>
2022-04-08 21:00:01 +0200

    init

---
+++ Assets/Resources/RosSubscriberExample.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using Unity.Robotics.ROSTCPConnector;
+using RosColor = RosMessageTypes.UnityRoboticsDemo.UnityColorMsg;
+
+public class RosSubscriberExample : MonoBehaviour
+{
+    public GameObject cube;
+
+    void Start()
+    {
+        Debug.Log("TEst");
+        ROSConnection.GetOrCreateInstance().Subscribe<RosColor>("color", ColorChange);
+    }
+
+    void ColorChange(RosColor colorMessage)
+    {
+        Debug.Log("TEst");
+        cube.GetComponent<Renderer>().material.color = new Color32((byte)colorMessage.r, (byte)colorMessage.g, (byte)colorMessage.b, (byte)colorMessage.a);
+    }
+}
