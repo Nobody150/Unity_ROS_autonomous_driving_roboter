fc74a5973b389e2a48a87f934f1a86475a7b2efc
Phil Schneider <Phil.Schneider@fh-zwickau.de>
2022-06-02 10:24:54 +0200

    Add other Race Track

---
+++ Assets/Racetrack/race-track-lake/moveCamera.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moveCamera : MonoBehaviour
+{
+    public float factor = 1.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float vertAngle = factor * Input.GetAxis("CamVertical");
+        float horizAngle = factor * Input.GetAxis("CamHorizontal");
+        
+        this.transform.Rotate(new Vector3(0,1,0), horizAngle);
+        this.transform.Rotate(new Vector3(1,0,0), vertAngle);
+
+    }
+}
